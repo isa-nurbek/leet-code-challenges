c49644b709a478662646f78997b807497277017f
Isradin <isradin.110593@gmail.com>
2025-01-20 21:14:38 +0300

    Fix issuies in while loop

--- Arrays/Validate Subsequence/solution_1.py
+++ Arrays/Validate Subsequence/solution_1.py
@@ -35,7 +35,7 @@ def is_valid_subsequence(array, sequence):
     arr_idx = 0
     seq_idx = 0
 
-    while arr_idx < len(array) and seq_idx < len(array):
+    while arr_idx < len(array) and seq_idx < len(sequence):
         if array[arr_idx] == sequence[seq_idx]:
             seq_idx += 1
         arr_idx += 1
