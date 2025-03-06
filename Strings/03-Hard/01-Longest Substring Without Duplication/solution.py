eb4035c14ca54240a09963648bb72a9875adfd2a
Isradin <isradin.110593@gmail.com>
2025-03-07 01:56:07 +0300

    Fix issue

--- Strings/03-Hard/01-Longest Substring Without Duplication/solution.py
+++ Strings/03-Hard/01-Longest Substring Without Duplication/solution.py
@@ -20,7 +20,8 @@ string = "kilimandjaro"
 
 ## Optimal Time & Space Complexity:
 ```
-`O(n)` time | `O(min(n, a))` space - where `n` is the length of the input string and `a` is the length of the character alphabet represented in the input string.
+`O(n)` time | `O(min(n, a))` space - where `n` is the length of the input string
+and `a` is the length of the character alphabet represented in the input string.
 ```
 
 """
