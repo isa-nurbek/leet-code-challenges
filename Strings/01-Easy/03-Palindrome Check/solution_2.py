8be99bd976631da78775e0598a2deda33d1f830c
Isradin <isradin.110593@gmail.com>
2025-03-13 00:03:24 +0300

    Fix issues & update code

--- Strings/01-Easy/03-Palindrome Check/solution_2.py
+++ Strings/01-Easy/03-Palindrome Check/solution_2.py
@@ -1,26 +1,24 @@
-# Description:
+# Problem Description:
 
 """
-                                         Palindrome Check
+                                          Palindrome Check
 
 Write a function that takes in a non-empty string and that returns a boolean representing whether the string is a palindrome.
 
 A palindrome is defined as a string that's written the same forward and backward. Note that single-character strings are palindromes.
 
-## Sample Input:
 
+## Sample Input:
 ```
 string = "abcdcba"
 ```
 
 ## Sample Output:
-
 ```
 true // it's written the same forward and backward
 ```
 
-## Optimal Space & Time Complexity:
-
+## Optimal Time & Space Complexity:
 ```
 O(n) time | O(1) space - where `n` is the length of the input string.
 ```
@@ -51,7 +49,7 @@ print(isPalindrome("Madam"))  # False
 
 # =========================================================================================================================== #
 
-# Big O:
+# Big O Analysis:
 
 """
 ## Time and Space Complexity Analysis
@@ -83,7 +81,7 @@ Thus, the overall space complexity is:
 
 # =========================================================================================================================== #
 
-# Code Explanation:
+# Detailed Code Explanation:
 
 """
 ### Code Breakdown and Explanation
