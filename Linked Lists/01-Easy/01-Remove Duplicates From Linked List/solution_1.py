1359479708c0f0c847fd5a18551d3e8e2f722ea2
Isradin <isradin.110593@gmail.com>
2025-04-06 05:03:42 +0300

    Fix issues & Update code

--- Linked Lists/01-Easy/01-Remove Duplicates From Linked List/solution_1.py
+++ Linked Lists/01-Easy/01-Remove Duplicates From Linked List/solution_1.py
@@ -3,14 +3,19 @@
 """
                                             # Remove Duplicates From Linked List
 
-You're given the head of a Singly Linked List whose nodes are in sorted order with respect to their values. Write a function that returns a modified version of the Linked List that doesn't contain any nodes with duplicate values. The Linked List should be modified in place (i.e., you shouldn't create a brand new list), and the modified Linked List should still have its nodes sorted with respect to their values.
+You're given the head of a Singly Linked List whose nodes are in sorted/unsorted order with respect to their values. Write a
+function that returns a modified version of the Linked List that doesn't contain any nodes with duplicate values. The Linked List
+should be modified in place (i.e., you shouldn't create a brand new list), and the modified Linked List should still have its
+nodes sorted/unsorted with respect to their values.
 
-Each `LinkedList` node has an integer `value` as well as a `next` node pointing to the next node in the list or to `None` / `null` if it's the tail of the list.
+Each `LinkedList` node has an integer `value` as well as a `next` node pointing to the next node in the list or to `None` / `null`
+if it's the tail of the list.
 
 
-## Sample Input:
+
+## Sample Input Sorted Linked List:
 ```
-linked_list = {
+sorted_linked_list_dict = {
     "head": "1",
     "nodes": [
         {"id": "1", "next": "1-2", "value": 1},
@@ -29,7 +34,7 @@ linked_list = {
 The List Looks Like: [1] -> [1] -> [3] -> [4] -> [4] -> [4] -> [5] -> [6] -> [6] -> None // the head node with value 1
 ```
 
-## Sample Output:
+## Sample Output Sorted Linked List:
 ```
 1 -> 3 -> 4 -> 5 -> 6 -> None // the head node with value 1
 ```
@@ -126,7 +131,7 @@ def print_linked_list(linked_list):
 
 
 # Your input data
-linked_list_dict = {
+sorted_linked_list_dict = {
     "head": "1",
     "nodes": [
         {"id": "1", "next": "1-2", "value": 1},
@@ -145,7 +150,7 @@ linked_list_dict = {
 # Test Case:
 
 # Build the linked list from dictionary
-linked_list = build_linked_list(linked_list_dict)
+linked_list = build_linked_list(sorted_linked_list_dict)
 
 # Remove duplicates
 result = remove_duplicates_from_linked_list(linked_list)
