5684900df4de643ec28862851b49b38788e568a5
Isradin <isradin.110593@gmail.com>
2025-04-13 04:47:13 +0300

    Add explanation comments to code

--- Recursion/02-Medium/01-Permutations/solution_2.py
+++ Recursion/02-Medium/01-Permutations/solution_2.py
@@ -33,22 +33,51 @@ O(n * n!) time | O(n * n!) space - where `n` is the length of the input array.
 
 # O(n * n!) time | O(n * n!) space
 def get_permutations(array):
-    permutations = []
-    permutations_helper(0, array, permutations)
+    """Main function to generate all permutations of an array.
+
+    Args:
+        array: The input array to generate permutations for
+
+    Returns:
+        A list containing all possible permutations of the input array
+    """
+    permutations = []  # This will store all the permutations we generate
+    permutations_helper(0, array, permutations)  # Start the recursive process
     return permutations
 
 
 def permutations_helper(i, array, permutations):
+    """Recursive helper function to generate permutations.
+
+    This works by swapping elements to generate all possible orderings.
+
+    Args:
+        i: Current index we're fixing in the permutation
+        array: The array we're generating permutations for
+        permutations: List to store the resulting permutations
+    """
+    # Base case: If we've reached the last element, we have a complete permutation
     if i == len(array) - 1:
-        permutations.append(array[:])
+        permutations.append(array[:])  # Add a copy of the current array state
     else:
+        # Recursive case: Generate permutations for remaining elements
         for j in range(i, len(array)):
+            # Swap current element with element at index j
             swap(array, i, j)
+            # Recursively generate permutations for the remaining positions
             permutations_helper(i + 1, array, permutations)
+            # Swap back (backtrack) to restore original array for next iteration
             swap(array, i, j)
 
 
 def swap(array, i, j):
+    """Helper function to swap two elements in an array in-place.
+
+    Args:
+        array: The array containing elements to swap
+        i: Index of first element
+        j: Index of second element
+    """
     array[i], array[j] = array[j], array[i]
 
 
