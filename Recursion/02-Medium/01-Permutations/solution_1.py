ffbbcca74bae9342d47726e1b14b8323ea6dfd8c
Isradin <isradin.110593@gmail.com>
2025-04-13 04:26:14 +0300

    Add explanation comments to code

--- Recursion/02-Medium/01-Permutations/solution_1.py
+++ Recursion/02-Medium/01-Permutations/solution_1.py
@@ -34,18 +34,41 @@ O(n * n!) time | O(n * n!) space - where `n` is the length of the input array.
 # Upper Bound: O(n² * n!) time | O(n * n!) space
 # Roughly: O(n * n!) time | O(n * n!) space
 def get_permutations(array):
-    permutations = []
-    permutations_helper(array, [], permutations)
+    """Main function to get all permutations of an array.
+
+    Args:
+        array: The input array to generate permutations for
+
+    Returns:
+        A list of all possible permutations of the input array
+    """
+    permutations = []  # This will store all the permutations we generate
+    permutations_helper(array, [], permutations)  # Start the recursive process
     return permutations
 
 
 def permutations_helper(array, current_permutation, permutations):
+    """Recursive helper function to generate permutations.
+
+    Args:
+        array: The remaining elements to permute
+        current_permutation: The permutation being built in the current recursion path
+        permutations: The list that accumulates all complete permutations
+    """
+    # Base case: if array is empty and current_permutation has elements
     if not len(array) and len(current_permutation):
+        # Add the completed permutation to our results
         permutations.append(current_permutation)
     else:
+        # Recursive case: build permutations by choosing each remaining element
         for i in range(len(array)):
+            # Create new array without the current element (array[i])
             new_array = array[:i] + array[i + 1 :]
+
+            # Add current element to the permutation we're building
             new_permutation = current_permutation + [array[i]]
+
+            # Recurse with the remaining elements and updated permutation
             permutations_helper(new_array, new_permutation, permutations)
 
 
