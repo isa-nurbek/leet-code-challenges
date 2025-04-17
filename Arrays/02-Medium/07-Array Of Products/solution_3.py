12d386c0330fe1331b80602f395fbb97f37ac220
Isradin <isradin.110593@gmail.com>
2025-04-17 04:39:14 +0300

    Resolve issues and refresh data

--- Arrays/02-Medium/07-Array Of Products/solution_3.py
+++ Arrays/02-Medium/07-Array Of Products/solution_3.py
@@ -38,7 +38,7 @@ O(n) time | O(n) space - where `n` is the length of the input array.
 # Solution:
 
 
-# O(n) time | O(n) space - where `n` is the length of the input array
+# O(n) time | O(n) space
 def array_of_products(array):
     # Initialize a list called 'products' with the same length as the input array,
     # where each element is set to 1. This will store the final product values.
