51956c97c9556133d170fff951af02d8cdad28d0
Isradin <isradin.110593@gmail.com>
2025-04-17 04:39:38 +0300

    Resolve issues and refresh data

--- Arrays/02-Medium/08-First Duplicate Value/solution_1.py
+++ Arrays/02-Medium/08-First Duplicate Value/solution_1.py
@@ -49,7 +49,7 @@ O(n) time | O(1) space - where `n` is the length of the input array.
 # Solution:
 
 
-# O(n^2) time | O(1) space - where `n` is the length of the input array.
+# O(n^2) time | O(1) space
 def first_duplicate_value(array):
     # Initialize the variable to store the index of the first duplicate found.
     # We start with the length of the array, which is an invalid index, to indicate no duplicate has been found yet.
