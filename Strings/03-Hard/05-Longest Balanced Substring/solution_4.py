130888db6eccd4bfdbfe9ad6caebdba06716f909
Isradin <isradin.110593@gmail.com>
2025-04-20 04:15:21 +0300

    Resolve issues and refresh data

--- Strings/03-Hard/05-Longest Balanced Substring/solution_4.py
+++ Strings/03-Hard/05-Longest Balanced Substring/solution_4.py
@@ -33,7 +33,7 @@ O(n) time | O(1) space - where `n` is the length of the input string.
 # Solution:
 
 
-# O(n) time | O(1) space - where `n` is the length of the input string
+# O(n) time | O(1) space
 def longest_balanced_substring(string):
     # The function returns the maximum length of a balanced substring by checking
     # both directions (left-to-right and right-to-left) and returning the larger value.
