0165d063b1e29246c3302ef71f8f0fb538131c4f
Isradin <isradin.110593@gmail.com>
2025-04-21 03:42:47 +0300

    Implement solution_1 in Python

---
+++ Recursion/02-Medium/04-Staircase Traversal/solution_1.py
@@ -0,0 +1,65 @@
+# Problem Description:
+
+"""
+
+                                          Staircase Traversal
+
+You're given two positive integers representing the height of a staircase and the maximum number of steps that you can
+advance up the staircase at a time. Write a function that returns the number of ways in which you can climb the staircase.
+
+For example, if you were given a staircase of `height = 3` and `max_steps = 2` you could climb the staircase in 3 ways. You
+could take **1 step, 1 step, then 1 step**, you could also take **1 step, then 2 steps**, and you could take **2 steps,
+then 1 step**.
+
+> Note that `max_steps <= height` will always be true.
+
+
+## Sample Input
+```
+height = 4
+max_steps = 2
+```
+
+## Sample Output
+```
+5
+// You can climb the staircase in the following ways:
+// 1, 1, 1, 1
+// 1, 1, 2
+// 1, 2, 1
+// 2, 1, 1
+// 2, 2
+```
+
+## Optimal Time & Space Complexity:
+```
+O(n) time | O(n) space - where `n` is the height of the staircase.
+```
+"""
+
+# =========================================================================================================================== #
+
+# Solution:
+
+
+# O(kⁿ) time | O(n) space
+def staircase_traversal(height, max_steps):
+    return number_of_ways_to_top(height, max_steps)
+
+
+def number_of_ways_to_top(height, max_steps):
+    if height <= 1:
+        return 1
+
+    number_of_ways = 0
+    for step in range(1, min(max_steps, height) + 1):
+        number_of_ways += number_of_ways_to_top(height - step, max_steps)
+
+    return number_of_ways
+
+
+# Test Cases:
+
+print(staircase_traversal(4, 2))  #  Output: 5
+print(staircase_traversal(10, 1))  #  Output: 1
+print(staircase_traversal(6, 3))  #  Output: 24
