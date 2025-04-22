2c563faab0f7841fff867a2e67949c5a8408cf0d
Isradin <isradin.110593@gmail.com>
2025-04-22 04:50:31 +0300

    Delete info

--- Recursion/02-Medium/04-Staircase Traversal/solution_4.py
+++ Recursion/02-Medium/04-Staircase Traversal/solution_4.py
@@ -3,12 +3,3 @@
 """
 
                                           Staircase Traversal
-
-You're given two positive integers representing the height of a staircase and the maximum number of steps that you can
-advance up the staircase at a time. Write a function that returns the number of ways in which you can climb the staircase.
-
-For example, if you were given a staircase of `height = 3` and `max_steps = 2` you could climb the staircase in 3 ways. You
-could take **1 step, 1 step, then 1 step**, you could also take **1 step, then 2 steps**, and you could take **2 steps,
-then 1 step**.
-
-> Note that `max_steps <= height` will always be true.
