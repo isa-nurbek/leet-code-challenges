211b79bac63ff3ba9eb2ebeea3926037de6a24c5
Isradin <108975888+isa-nurbek@users.noreply.github.com>
2025-04-28 22:48:24 +0300

    Update solution_2.py

--- Arrays/03-Hard/04-Min Rewards/solution_2.py
+++ Arrays/03-Hard/04-Min Rewards/solution_2.py
@@ -26,7 +26,9 @@ scores = [8, 4, 2, 1, 3, 6, 7, 9, 5]
 
 ## Sample Output:
 ```
-25 // you would give out the following rewards: [4, 3, 2, 1, 2, 3, 4, 5, 1]
+25 
+
+// You would give out the following rewards: [4, 3, 2, 1, 2, 3, 4, 5, 1]
 ```
 
 ## Optimal Time & Space Complexity:
