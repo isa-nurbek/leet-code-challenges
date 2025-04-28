0a7c05c409245b827acdde1655f6c0b91cd76861
Isradin <108975888+isa-nurbek@users.noreply.github.com>
2025-04-28 23:23:24 +0300

    Update solution.py

--- Recursion/01-Easy/02-Product Sum/solution.py
+++ Recursion/01-Easy/02-Product Sum/solution.py
@@ -23,7 +23,9 @@ array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
 
 ## Sample Output
 ```
-12 // calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
+12 
+
+// Calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
 ```
 
 ## Optimal Time & Space Complexity:
