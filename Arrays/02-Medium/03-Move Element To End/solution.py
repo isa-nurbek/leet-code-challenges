016adabaacb0105731af75380267f4d1346b22ed
Isradin <108975888+isa-nurbek@users.noreply.github.com>
2025-04-28 22:27:31 +0300

    Update solution.py

--- Arrays/02-Medium/03-Move Element To End/solution.py
+++ Arrays/02-Medium/03-Move Element To End/solution.py
@@ -19,7 +19,9 @@ to_move = 2
 
 ## Sample Output:
 ```
-[1, 3, 4, 2, 2, 2, 2, 2] // the numbers 1, 3, and 4 could be ordered differently.
+[1, 3, 4, 2, 2, 2, 2, 2]
+
+// The numbers 1, 3, and 4 could be ordered differently
 ```
 
 ## Optimal Time & Space Complexity:
