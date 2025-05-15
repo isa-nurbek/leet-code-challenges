b30c9114d7a5b52b4427dac1d9d9b2eb18c1a728
Isradin <108975888+isa-nurbek@users.noreply.github.com>
2025-05-15 05:28:01 +0300

    Update solution.py

--- Linked Lists/02-Medium/04-Reverse Linked List/solution.py
+++ Linked Lists/02-Medium/04-Reverse Linked List/solution.py
@@ -45,6 +45,7 @@ class LinkedList:
         self.next = None  # Pointer to the next node (initially None)
 
 
+# Helper function to convert dictionary to LinkedList
 def build_linked_list(data):
     """Builds a linked list from a dictionary representation.
 
