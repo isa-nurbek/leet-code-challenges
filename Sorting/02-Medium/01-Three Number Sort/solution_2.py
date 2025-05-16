8e969f0c97b78b826307ec1805da55675357d102
Isradin <isradin.110593@gmail.com>
2025-05-16 13:27:36 +0300

    Implement solution_2 in Python

---
+++ Sorting/02-Medium/01-Three Number Sort/solution_2.py
@@ -0,0 +1,74 @@
+# Problem Description:
+
+"""
+                                               Three Number Sort
+
+You're given an array of integers and another array of three distinct integers. The first array is guaranteed to only contain
+integers that are in the second array, and the second array represents a desired order for the integers in the first array. For
+example, a second array of `[x, y, z]` represents a desired order of `[x, x, ..., x, y, y, ..., y, z, z, ..., z]` in the first array.
+
+Write a function that sorts the first array according to the desired order in the second array.
+
+The function should perform this in place (i.e., it should mutate the input array), and it shouldn't use any auxiliary space
+(i.e., it should run with constant space: `O(1)` space).
+
+> Note that the desired order won't necessarily be ascending or descending and that the first array won't necessarily contain all
+three integers found in the second array—it might only contain one or two.
+
+
+## Sample Input:
+```
+array = [1, 0, 0, -1, -1, 0, 1, 1]
+order = [0, 1, -1]
+```
+
+## Sample Output:
+```
+[0, 0, 0, 1, 1, 1, -1, -1]
+```
+
+## Optimal Time & Space Complexity:
+```
+O(n) time | O(1) space - where `n` is the length of the array.
+```
+
+"""
+
+# =========================================================================================================================== #
+
+# Solution:
+
+
+# O(n) time | O(1) space
+def three_number_sort(array, order):
+    first_value = order[0]
+    second_value = order[1]
+
+    first_idx, second_idx, third_idx = 0, 0, len(array) - 1
+
+    while second_idx <= third_idx:
+        value = array[second_idx]
+
+        if value == first_value:
+            array[second_idx], array[first_idx] = array[first_idx], array[second_idx]
+            first_idx += 1
+            second_idx += 1
+        elif value == second_value:
+            second_idx += 1
+        else:
+            array[second_idx], array[third_idx] = array[third_idx], array[second_idx]
+            third_idx -= 1
+
+    return array
+
+
+# Test Cases:
+
+print(three_number_sort([1, 0, 0, -1, -1, 0, 1, 1], [0, 1, -1]))
+# Output: [0, 0, 0, 1, 1, 1, -1, -1]
+
+print(three_number_sort([7, 8, 9, 7, 8, 9, 9, 9, 9, 9, 9, 9], [8, 7, 9]))
+# Output: [8, 8, 7, 7, 9, 9, 9, 9, 9, 9, 9, 9]
+
+print(three_number_sort([], [0, 7, 9]))
+# Output: []
