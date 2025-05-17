d5182bfc79ee03f07d2235f1c7f9d7c749fd2239
Isradin <isradin.110593@gmail.com>
2025-05-17 21:16:29 +0300

    Implement solution in Python

---
+++ Sorting/02-Medium/03-Heap Sort/solution.py
@@ -0,0 +1,75 @@
+# Problem Description:
+
+"""
+                                               Heap Sort
+
+Write a function that takes in an array of integers and returns a `sorted version` of that array. Use the `Heap Sort` algorithm
+to sort the array.
+
+
+## Sample Input:
+```
+array = [8, 5, 2, 9, 5, 6, 3]
+```
+
+## Sample Output:
+```
+[2, 3, 5, 5, 6, 8, 9]
+```
+
+## Optimal Time & Space Complexity:
+```
+Best: O(n log(n)) time | O(1) space - where n is the length of the input array.
+Average: O(n log(n)) time | O(1) space - where n is the length of the input array.
+Worst: O(n log(n)) time | O(1) space - where n is the length of the input array.
+```
+
+"""
+
+# =========================================================================================================================== #
+
+# Solution:
+
+
+# Best: O(n log(n)) time | O(1) space
+# Average: O(n log(n)) time | O(1) space
+# Worst: O(n log(n)) time | O(1) space
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # Swap
+        heapify(arr, n, largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+    return arr
+
+
+# Test Cases:
+
+print(heap_sort([8, 5, 2, 9, 5, 6, 3]))
+# Output: [2, 3, 5, 5, 6, 8, 9]
+
+print(heap_sort([-4, 5, 10, 8, -10, -6, -4, -2, -5, 3, 5, -4, -5, -1, 1, 6, -7]))
+# Output: [-10, -7, -6, -5, -5, -4, -4, -4, -2, -1, 1, 3, 5, 5, 6, 8, 10]
+
+print(heap_sort([2, 1]))
+# Output: [1, 2]
