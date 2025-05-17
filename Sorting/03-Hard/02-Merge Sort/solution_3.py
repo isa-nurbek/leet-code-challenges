2adaa640d1106f6b7d115fdad4eb78a3d2301096
Isradin <isradin.110593@gmail.com>
2025-05-18 01:31:08 +0300

    Implement solution_3 iterative approach in Python

---
+++ Sorting/03-Hard/02-Merge Sort/solution_3.py
@@ -0,0 +1,92 @@
+# Problem Description:
+
+"""
+                                                Merge Sort
+
+Write a function that takes in an array of integers and returns a `sorted` version of that array. Use the `Merge Sort` algorithm
+to sort the array.
+
+
+## Sample Input:
+```
+array = [8, 5, 2, 9, 5, 6, 3]
+```
+
+## Sample Output:
+```
+[2, 3, 5, 5, 6, 8, 9]
+```
+
+## Optimal Time & Space Complexity:
+```
+Best: O(n log(n)) time | O(n) space - where `n` is the length of the input array.
+Average: O(n log(n)) time | O(n) space - where `n` is the length of the input array.
+Worst: O(n log(n)) time | O(n) space - where `n` is the length of the input array.
+```
+
+"""
+
+# =========================================================================================================================== #
+
+# Solution:
+
+
+# Best: O(n log(n)) time | O(n) space
+# Average: O(n log(n)) time | O(n) space
+# Worst: O(n log(n)) time | O(n) space
+def merge_sort_iterative(arr):
+    n = len(arr)
+    temp = [0] * n
+    size = 1
+
+    while size < n:
+        for left_start in range(0, n, size * 2):
+            mid = min(left_start + size - 1, n - 1)
+            right_end = min(left_start + 2 * size - 1, n - 1)
+
+            if mid < right_end:
+                merge(arr, temp, left_start, mid, right_end)
+
+        size *= 2
+
+    return arr
+
+
+def merge(arr, temp, left, mid, right):
+    i = left
+    j = mid + 1
+    k = left
+
+    while i <= mid and j <= right:
+        if arr[i] <= arr[j]:
+            temp[k] = arr[i]
+            i += 1
+        else:
+            temp[k] = arr[j]
+            j += 1
+        k += 1
+
+    while i <= mid:
+        temp[k] = arr[i]
+        i += 1
+        k += 1
+
+    while j <= right:
+        temp[k] = arr[j]
+        j += 1
+        k += 1
+
+    for k in range(left, right + 1):
+        arr[k] = temp[k]
+
+
+# Test Cases:
+
+print(merge_sort_iterative([8, 5, 2, 9, 5, 6, 3]))
+# Output: [2, 3, 5, 5, 6, 8, 9]
+
+print(merge_sort_iterative([38, 27, 43, 3, 9, 82, 10]))
+# Output: [3, 9, 10, 27, 38, 43, 82]
+
+print(merge_sort_iterative([5, -2, 2, -8, 3, -10, -6, -1, 2, -2, 9, 1, 1]))
+# Output: [-10, -8, -6, -2, -2, -1, 1, 1, 2, 2, 3, 5, 9]
