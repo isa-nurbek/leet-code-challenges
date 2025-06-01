1f7f4676256cbb682c1c1321c85e02b08bd6dbc5
Isradin <isradin.110593@gmail.com>
2025-06-01 11:06:49 +0300

    Implement solution_5 in Python

---
+++ Dynamic Programming/01-Easy/05-Number Of Ways To Traverse Graph/solution_5.py
@@ -0,0 +1,68 @@
+# Problem Description:
+
+"""
+                                               Number Of Ways To Traverse Graph
+
+You're given two positive integers representing the `width` and `height` of a grid-shaped, rectangular graph. Write a function that
+returns the number of ways to reach the bottom right corner of the graph when starting at the top left corner. Each move you take
+must either go down or right. In other words, you can never move up or left in the graph.
+
+For example, given the graph illustrated below, with `width = 2` and `height = 3`, there are three ways to reach the bottom right
+corner when starting at the top left corner:
+
+```
+ _ _
+|_|_|
+|_|_|
+|_|_|
+
+```
+
+1. Down, Down, Right
+2. Right, Down, Down
+3. Down, Right, Down
+
+> Note: you may assume that `width * height >= 2`. In other words, the graph will never be a `1x1 grid`.
+
+
+## Sample Input:
+```
+width = 4
+height = 3
+```
+
+## Sample Output:
+```
+10
+```
+
+## Optimal Time & Space Complexity:
+```
+O(n + m) time | O(1) space - where `n` is the width of the graph and `m` is the height.
+```
+
+"""
+
+# =========================================================================================================================== #
+
+# Solution:
+
+import math
+
+
+# O(w + h) time | O(1) space
+# Combinatorial Optimization (Mathematical Formula)
+def number_of_ways_to_traverse_graph(width, height):
+    return math.comb(width + height - 2, width - 1)
+
+
+# Test Cases:
+
+print(number_of_ways_to_traverse_graph(4, 3))
+# Output: 10
+
+print(number_of_ways_to_traverse_graph(3, 2))
+# Output: 3
+
+print(number_of_ways_to_traverse_graph(2, 2))
+# Output: 2
