e064aa08619d361a1803f0aaf3a664bb48a26c53
Isradin <isradin.110593@gmail.com>
2025-06-02 13:53:45 +0300

    Implement solution_1 in Python

---
+++ Dynamic Programming/02-Medium/02-Longest Common Subsequence/solution_1.py
@@ -0,0 +1,63 @@
+# Problem Description:
+
+"""
+                                              Longest Common Subsequence
+
+Write a function that takes in two strings and returns their longest common subsequence.
+
+A subsequence of a string is a set of characters that aren't necessarily adjacent in the string but that are in the same order as
+they appear in the string. For instance, the characters `["a", "c", "d"]` form a subsequence of the string `"abcd"`, and so do the
+characters `["b", "d"]`.
+
+> Note that a single character in a string and the string itself are both valid subsequences of the string.
+
+You can assume that there will only be one longest common subsequence.
+
+
+## Sample Input:
+```
+str_1 = "ZXVVYZW"
+str_2 = "XKYKZPW"
+```
+
+## Sample Output:
+```
+["X", "Y", "Z", "W"]
+```
+
+## Optimal Time & Space Complexity:
+```
+O(n * m) time | O(n * m) space - where `n` and `m` are the lengths of the two input strings.
+```
+
+"""
+
+# =========================================================================================================================== #
+
+# Solution:
+
+
+# O(nm*min(n, m)) time | O(nm*min(n, m)) space
+def longest_common_subsequence(str_1, str_2):
+    lcs = [[[] for x in range(len(str_1) + 1)] for y in range(len(str_2) + 1)]
+
+    for i in range(1, len(str_2) + 1):
+        for j in range(1, len(str_1) + 1):
+            if str_2[i - 1] == str_1[j - 1]:
+                lcs[i][j] = lcs[i - 1][j - 1] + [str_2[i - 1]]
+            else:
+                lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1], key=len)
+
+    return lcs[-1][-1]
+
+
+# Test Cases:
+
+print(longest_common_subsequence("ZXVVYZW", "XKYKZPW"))
+# Output: ['X', 'Y', 'Z', 'W']
+
+print(longest_common_subsequence("", "ABCDEFG"))
+# Output: []
+
+print(longest_common_subsequence("ABCDEFG", "APPLES"))
+# Output: ['A', 'E']
