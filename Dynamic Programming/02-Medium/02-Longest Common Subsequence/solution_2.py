c0289ac584e9ff84376a799909e14efcaa69cde0
Isradin <isradin.110593@gmail.com>
2025-06-02 17:51:25 +0300

    Add explanation comments to solution code

--- Dynamic Programming/02-Medium/02-Longest Common Subsequence/solution_2.py
+++ Dynamic Programming/02-Medium/02-Longest Common Subsequence/solution_2.py
@@ -39,33 +39,43 @@ O(n * m) time | O(n * m) space - where `n` and `m` are the lengths of the two in
 
 # O(m * n) time | O(n) space
 def longest_common_subsequence(str_1, str_2):
-    if len(str_1) < len(str_2):
-        str_1, str_2 = str_2, str_1
-
+    # Get lengths of both input strings
     m, n = len(str_1), len(str_2)
-    dp_prev = [0] * (n + 1)
 
-    for i in range(1, m + 1):
-        dp_curr = [0] * (n + 1)
-        for j in range(1, n + 1):
+    # Create a DP (Dynamic Programming) table of size (m+1) x (n+1) initialized with 0s
+    # dp[i][j] will store the length of LCS of str_1[0..i-1] and str_2[0..j-1]
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    # Fill the DP table
+    for i in range(1, m + 1):  # for each character in str_1
+        for j in range(1, n + 1):  # for each character in str_2
             if str_1[i - 1] == str_2[j - 1]:
-                dp_curr[j] = dp_prev[j - 1] + 1
+                # If characters match, take diagonal value + 1
+                dp[i][j] = dp[i - 1][j - 1] + 1
             else:
-                dp_curr[j] = max(dp_prev[j], dp_curr[j - 1])
-        dp_prev = dp_curr
+                # If no match, take maximum of top or left cell
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-    lcs = []
-    i, j = m, n
+    # Now we'll reconstruct the LCS from the DP table
+    lcs = []  # This will store the characters of the LCS
+    i, j = m, n  # Start from the bottom-right corner of the DP table
+
+    # Backtrack through the DP table
     while i > 0 and j > 0:
         if str_1[i - 1] == str_2[j - 1]:
+            # If characters match, it's part of LCS
             lcs.append(str_1[i - 1])
+            # Move diagonally up-left
             i -= 1
             j -= 1
-        elif dp_prev[j] > dp_curr[j - 1]:
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            # If top cell has higher value than left cell, move up
             i -= 1
         else:
+            # Otherwise move left
             j -= 1
 
+    # The LCS was built in reverse order, so reverse it before returning
     return lcs[::-1]
 
 
