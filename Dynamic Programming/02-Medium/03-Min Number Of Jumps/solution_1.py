87d48bfb1ed67920642cd0b8fff2155aed04b0a9
Isradin <isradin.110593@gmail.com>
2025-06-02 18:19:46 +0300

    Implement solution in Python

---
+++ Dynamic Programming/02-Medium/03-Min Number Of Jumps/solution_1.py
@@ -0,0 +1,60 @@
+# Problem Description:
+
+"""
+                                            Min Number Of Jumps
+
+You're given a `non-empty` array of positive integers where each integer represents the maximum number of steps you can take
+forward in the array. For example, if the element at index `1` is `3`, you can go from index `1` to index `2`, `3`, or `4`.
+
+Write a function that returns the minimum number of jumps needed to reach the final index.
+
+> Note that jumping from index `i` to index `i + x` always constitutes one jump, no matter how large `x` is.
+
+
+## Sample Input:
+```
+array = [3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]
+```
+
+## Sample Output:
+```
+4
+
+// 3 --> (4 or 2) --> (2 or 3) --> 7 --> 3
+```
+
+## Optimal Time & Space Complexity:
+```
+O(n) time | O(1) space - where `n` is the length of the input array.
+```
+
+"""
+
+# =========================================================================================================================== #
+
+# Solution:
+
+
+# O(n²) time | O(n) space
+def min_number_of_jumps(array):
+    jumps = [float("inf") for x in array]
+    jumps[0] = 0
+
+    for i in range(1, len(array)):
+        for j in range(0, i):
+            if array[j] >= i - j:
+                jumps[i] = min(jumps[j] + 1, jumps[i])
+
+    return jumps[-1]
+
+
+# Test Cases:
+
+print(min_number_of_jumps([3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]))
+# Output: 4
+
+print(min_number_of_jumps([2, 1, 2, 3, 1]))
+# Output: 2
+
+print(min_number_of_jumps([1]))
+# Output: 0
